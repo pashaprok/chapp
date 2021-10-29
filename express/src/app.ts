dcbc9856d16ef3ffe5297ebe5075dbc3cff44b89
pashaprok <pashaprok23@gmail.com>
2021-10-29 17:21:54 +0300

    initial commit

---
+++ express/src/app.ts
@@ -0,0 +1,25 @@
+import express, {Application, Request, Response} from 'express'
+import { HttpError } from "http-errors";
+
+const app: Application = express();
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response) => {
+  res.send('hello, world! i\'m chapp');
+});
+
+app.use((err: any, res: Response) => {
+  if (err instanceof HttpError) {
+    return res.status(err.statusCode || 500).json({
+      message: err.message,
+      code: err.statusCode,
+    })
+  }
+
+  return res.status(500).json({
+    message: err.toString(),
+    stack: err.stack,
+  });
+})
+
+export default app;
---
+++ express/src/server.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata'
+import 'express-async-errors'
+import { appConfig } from './config/app'
+import app from './app'
+
+app.listen(appConfig.port, () => {
+  console.log(`app running on port ${appConfig.port}...`);
+});
