08f2f366b06d7e529e85c66427568089443cc501
pashaprok <pashaprok23@gmail.com>
2021-11-01 11:15:54 +0200

    .

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,11 +1,11 @@
-import express, {Application, Request, Response} from 'express'
-import { HttpError } from "http-errors";
+import express, { Application, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
 
 const app: Application = express();
 app.use(express.json());
 
 app.use('/', (req: Request, res: Response) => {
-  res.send('hello, world! i\'m chapp');
+  res.send("hello, world! i'm chapp");
 });
 
 app.use((err: any, res: Response) => {
@@ -13,13 +13,13 @@ app.use((err: any, res: Response) => {
     return res.status(err.statusCode || 500).json({
       message: err.message,
       code: err.statusCode,
-    })
+    });
   }
 
   return res.status(500).json({
     message: err.toString(),
     stack: err.stack,
   });
-})
+});
 
 export default app;
--- express/src/server.ts
+++ express/src/server.ts
@@ -1,7 +1,7 @@
-import 'reflect-metadata'
-import 'express-async-errors'
-import { appConfig } from './config/app'
-import app from './app'
+import 'reflect-metadata';
+import 'express-async-errors';
+import { appConfig } from './config/app';
+import app from './app';
 
 app.listen(appConfig.port, () => {
   console.log(`app running on port ${appConfig.port}...`);
